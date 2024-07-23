799177c55e240ae7f5c3cdedba87c8595be897b5
Quan <tg6709032@gmail.com>
2024-07-23 20:25:33 +0000

    finished 3 and 4

--- flask/app.py
+++ flask/app.py
@@ -5,39 +5,30 @@ port = int(os.getenv('PORT'))
 
 @app.route('/')
 def hello():
-    response = make_response(
-        {
-            'response': 'hello, world!',
-            'status': 200
-        }
-    )
+    response = make_response('Hello, World!', 200)
     return response
 
 @app.route('/repeat')
 def repeat():
     input=request.args.get('input')
-    response = make_response(
-        {
-            'response': input,
-            'status': 200
-        }
-    )
+    response = make_response(input, 200)
     return response
 
 @app.route('/health')
+@app.route('/healthcheck')
 def health():
     # same as hello
-    response = make_response(
-    {
-        'body': 'OK',
-        'status': 200
-    }
-)
+    response = make_response('OK', 200)
     return response
+
+@app.route('/onpurpose')
+def badidea():
+    while True:
+        pass
 
 if __name__ == '__main__':
     # By default flask is only accessible from localhost.
     # Set this to '0.0.0.0' to make it accessible from any IP address
     # on your network (not recommended for production use)
 
-    app.run(host='0.0.0.0',port=port, debug=True)
+    app.run(host='0.0.0.0',port=port, debug=True, threaded=False)
